48c643595403ab9c70c68ac76c76b4909ded21b7
Coder Wally <97439397+coderwally@users.noreply.github.com>
2022-12-15 11:59:17 +0100

    inject into calmly

--- scripts/content.js
+++ scripts/content.js
@@ -1,6 +1,36 @@
 const insert = (content) => {
     
-    alert(`INSERT: ${content}`);
+    console.log(`INSERT: ${content}`);
+
+    // Find Calmly editor input section
+    const elements = document.getElementsByClassName('droid');
+
+    if (elements.length === 0) {
+      return;
+    }
+
+    const element = elements[0];
+
+    // Grab the first p tag so we can replace it with our injection
+    const pToRemove = element.childNodes[0];
+    pToRemove.remove();
+
+    const splitContent = content.split('\n');
+
+    // Wrap in p tags
+    splitContent.forEach((content) => {
+      const p = document.createElement('p');
+
+      if (content === '') {
+        const br = document.createElement('br');
+        p.appendChild(br);
+      } else {
+        p.textContent = content;
+      }
+
+      // Insert into HTML one at a time
+      element.appendChild(p);
+    });    
 
     return true;
 }
